1f5bf57927ae459f2cd7ffd64299d899e24ce45b
HorrorOTB <nicatabdullazade135@gmail.com>
2023-03-25 19:19:13 +0400

    formatted the code

--- quiz_front/src/utils/ExcludingRoutes.jsx
+++ quiz_front/src/utils/ExcludingRoutes.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import { Navigate, Outlet } from "react-router-dom";
+import { useToast } from "@chakra-ui/react";
 
 const ExcludingRoutes = () => {
     const token = localStorage.getItem("token");
+    const toast = useToast();
     return (
         //if request is to /auth and token is present, don't change the route
         token && window.location.pathname === "/auth" ? <Navigate to="/dashboard" /> : <Outlet />
