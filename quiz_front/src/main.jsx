eadc023fbfa23f97a21327988b9a2a0f20176b65
HorrorOTB <nicatabdullazade135@gmail.com>
2023-03-27 17:21:04 +0400

    changed the name to dashboard.jsx

--- quiz_front/src/main.jsx
+++ quiz_front/src/main.jsx
@@ -4,14 +4,12 @@ import Quiz from "./components/Quiz";
 import Stats from "./components/Stats"
 import Welcome from "./components/Welcome"
 import SignIn from "./components/SignIn";
-import QuestionManagement from "./components/QuestionManagement";
 import { ChakraProvider } from '@chakra-ui/react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "../src/assets/style.css"
-import Navbar from "./components/Navbar";
-import dotenv from "dotenv";
 import PrivateRoutes from "./utils/PrivateRoutes";
 import ExcludingRoutes from "./utils/ExcludingRoutes";
+import Dashboard from "./components/Dashboard";
 
 
 ReactDOM.createRoot(document.getElementById("root")).render(
@@ -19,7 +17,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
       <BrowserRouter>
         <Routes>
         <Route element={<PrivateRoutes/>}>
-            <Route path="dashboard/" element={<QuestionManagement />} />
+            <Route path="dashboard/" element={<Dashboard />} />
           </Route>
           <Route element={<ExcludingRoutes/>}>
             <Route path="auth/" element={<SignIn />} />
