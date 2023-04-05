6d90d1f36425b86c364d85bd1c2da4427a75549f
HorrorOTB <nicatabdullazade135@gmail.com>
2023-04-05 10:06:55 +0400

    partially fixed bugs

--- quiz_front/src/components/Dashboard.jsx
+++ quiz_front/src/components/Dashboard.jsx
@@ -5,6 +5,7 @@ import {
   UnorderedList,
   useDisclosure,
   useToast,
+  Spinner,
 } from "@chakra-ui/react";
 import { useState } from "react";
 import file_upload from "../assets/file.png";
@@ -48,6 +49,7 @@ export default function Dashboard() {
   const [filteredQuestions, setFilteredQuestions] = useState([]);
   const toast = useToast();
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isSubmitted, setIsSubmitted] = useState(false);
 
   const handleGroupChange = (event) => {
     const selectedGroup = event.target.value;
@@ -94,10 +96,19 @@ export default function Dashboard() {
   }, [teacherId]);
 
   useEffect(() => {
-    console.log(teacher);
-    console.log(teacherName);
-    console.log("filteredQuestions", filteredQuestions);
-  }, [filteredQuestions]);
+    const fetchQuestions = async () => {
+      const response = await getQuestions();
+      setFilteredQuestions(
+        response.filter(
+          (question) =>
+            question.groupId == selectedGroup &&
+            question.subjectId == selectedSubject
+        )
+      );
+    };
+    fetchQuestions();
+    isSubmitted && setIsSubmitted(false);
+  }, [isSubmitted]);
 
   const handleSearch = async () => {
     if (selectedGroup && selectedSubject) {
@@ -128,9 +139,8 @@ export default function Dashboard() {
   const handleSubmit = async () => {
     if (file && selectedGroup && selectedSubject) {
       const response = await sendFile(file, selectedGroup, selectedSubject);
-      handleSearch();
+      setIsSubmitted(true);
       console.log(response);
-      console.log(file);
     }
 
     if (selectedGroup && selectedSubject && !file) {
@@ -160,240 +170,244 @@ export default function Dashboard() {
   };
 
   return (
-    <div className="quiz-body">
-      <Navbar name={teacherName} surname={teacherSurname} />
-      <Flex
-        w={"80%"}
-        margin={"1em auto 0 auto"}
-        h={"80%"}
-        gap={1}
-        flexDirection={{ md: "row", base: "column" }}
-        justifyContent={"space-evenly"}
-      >
+    <>
+      <div className="quiz-body">
+        <Navbar name={teacherName} surname={teacherSurname} />
         <Flex
-          boxShadow={"4px 4px 1px black"}
-          border={"2px solid black"}
-          flexDirection={"column"}
-          p={"1em"}
-          gap={2}
-          maxW={"25%"}
-          minH={{ md: "", base: "25%" }}
-          minW={{ md: "25%", base: "100%" }}
-          overflowY={"auto"}
-          overflowX={"hidden"}
+          w={"80%"}
+          margin={"1em auto 0 auto"}
+          h={"80%"}
+          gap={1}
+          flexDirection={{ md: "row", base: "column" }}
+          justifyContent={"space-evenly"}
         >
-          {filteredQuestions &&
-            filteredQuestions.map((questions, key) => {
-              return (
-                <>
-                  <Flex
-                    cursor={"pointer"}
-                    onClick={onOpen}
-                    key={key.id}
-                    boxShadow={"4px 4px 0px black"}
-                    border={"2px solid black"}
-                    borderRadius={"0.3em"}
-                    backgroundColor={"white"}
-                    position={"relative"}
-                    bottom={0}
-                    transition={"bottom 0.1s ease-out"}
-                    _hover={{ bottom: "2px" }}
-                    justifyContent={"space-between"}
-                    p={"0.4em"}
+          <Flex
+            boxShadow={"4px 4px 1px black"}
+            border={"2px solid black"}
+            flexDirection={"column"}
+            p={"1em"}
+            gap={2}
+            maxW={"25%"}
+            minH={{ md: "", base: "25%" }}
+            minW={{ md: "25%", base: "100%" }}
+            overflowY={"auto"}
+            overflowX={"hidden"}
+          >
+            {filteredQuestions &&
+              filteredQuestions.map((questions, key) => {
+                return (
+                  <>
+                    <Flex
+                      cursor={"pointer"}
+                      onClick={onOpen}
+                      key={key.id}
+                      boxShadow={"4px 4px 0px black"}
+                      border={"2px solid black"}
+                      borderRadius={"0.3em"}
+                      backgroundColor={"white"}
+                      position={"relative"}
+                      bottom={0}
+                      transition={"bottom 0.1s ease-out"}
+                      _hover={{ bottom: "2px" }}
+                      justifyContent={"space-between"}
+                      p={"0.4em"}
+                    >
+                      <Box wordBreak={"break-word"}>{questions.filename}</Box>
+                      <Box>
+                        <CloseIcon
+                          cursor={"pointer"}
+                          onClick={(e) => {
+                            e.stopPropagation();
+                            handleDeleteQuestion(questions.id);
+                          }}
+                          fontSize={"0.7em"}
+                          color="black"
+                        />
+                      </Box>
+                    </Flex>
+                    <Modal isOpen={isOpen} onClose={onClose} isCentered>
+                      <ModalOverlay backgroundColor={"blackAlpha.300"} />
+                      <ModalContent>
+                        <ModalHeader>{questions.question}</ModalHeader>
+                        <ModalCloseButton />
+                        <ModalBody>
+                          <UnorderedList>
+                            {questions.Options.map((options) => (
+                              <ListItem>{options.option}</ListItem>
+                            ))}
+                          </UnorderedList>
+                          {questions.Options.map((options) => {
+                            if (options.is_correct) {
+                              return (
+                                <Text key={options.id}>
+                                  Correct Answer: {options.option}
+                                </Text>
+                              );
+                            }
+                          })}
+                        </ModalBody>
+                        <ModalFooter>
+                          <Button
+                            border={"2px solid black"}
+                            variant="outline"
+                            mr={3}
+                            onClick={onClose}
+                            _hover={{ backgroundColor: "white" }}
+                          >
+                            Close
+                          </Button>
+                        </ModalFooter>
+                      </ModalContent>
+                    </Modal>
+                  </>
+                );
+              })}
+          </Flex>
+
+          <Flex
+            justifyContent={"center"}
+            alignItems={"center"}
+            boxShadow={"4px 4px 1px black"}
+            border={"2px solid black"}
+            maxW={"50%"}
+            minH={{ md: "", base: "50%" }}
+            minW={{ md: "50%", base: "100%" }}
+            p={"10vw"}
+            flexWrap={"wrap"}
+          >
+            <Flex justifyContent={"center"} alignItems={"center"}>
+              <form>
+                <Flex
+                  flexDirection={"column"}
+                  justifyContent={"center"}
+                  alignItems={"center"}
+                >
+                  <Box
+                    onClick={() => document.getElementById("upload").click()}
                   >
-                    <Box wordBreak={"break-word"}>{questions.filename}</Box>
+                    <Image src={file_upload} w={"4em"} cursor="pointer" />
+                  </Box>
+                  <Flex justifyContent={"center"} alignItems={"center"}>
                     <Box>
-                      <CloseIcon
-                        cursor={"pointer"}
-                        onClick={(e) => {
-                          e.stopPropagation();
-                          handleDeleteQuestion(questions.id);
-                        }}
-                        fontSize={"0.7em"}
-                        color="black"
-                      />
+                      <Text wordBreak={"break-word"} htmlFor={"upload"}>
+                        {file ? file.name : "Choose File"}
+                      </Text>
                     </Box>
                   </Flex>
-                  <Modal isOpen={isOpen} onClose={onClose} isCentered>
-                    <ModalOverlay backgroundColor={"blackAlpha.300"} />
-                    <ModalContent>
-                      <ModalHeader>{questions.question}</ModalHeader>
-                      <ModalCloseButton />
-                      <ModalBody>
-                        <UnorderedList>
-                          {questions.Options.map((options) => (
-                            <ListItem>{options.option}</ListItem>
-                          ))}
-                        </UnorderedList>
-                        {questions.Options.map((options) => {
-                          if (options.is_correct) {
-                            return (
-                              <Text key={options.id}>
-                                Correct Answer: {options.option}
-                              </Text>
-                            );
-                          }
-                        })}
-                      </ModalBody>
-                      <ModalFooter>
-                        <Button
-                          border={"2px solid black"}
-                          variant="outline"
-                          mr={3}
-                          onClick={onClose}
-                          _hover={{ backgroundColor: "white" }}
-                        >
-                          Close
-                        </Button>
-                      </ModalFooter>
-                    </ModalContent>
-                  </Modal>
-                </>
-              );
-            })}
-        </Flex>
-
-        <Flex
-          justifyContent={"center"}
-          alignItems={"center"}
-          boxShadow={"4px 4px 1px black"}
-          border={"2px solid black"}
-          maxW={"50%"}
-          minH={{ md: "", base: "50%" }}
-          minW={{ md: "50%", base: "100%" }}
-          p={"10vw"}
-          flexWrap={"wrap"}
-        >
-          <Flex justifyContent={"center"} alignItems={"center"}>
-            <form>
-              <Flex
-                flexDirection={"column"}
-                justifyContent={"center"}
-                alignItems={"center"}
-              >
-                <Box onClick={() => document.getElementById("upload").click()}>
-                  <Image src={file_upload} w={"4em"} cursor="pointer" />
-                </Box>
-                <Flex justifyContent={"center"} alignItems={"center"}>
                   <Box>
-                    <Text wordBreak={"break-word"} htmlFor={"upload"}>
-                      {file ? file.name : "Choose File"}
-                    </Text>
+                    <Input
+                      id={"upload"}
+                      border={"none"}
+                      type="file"
+                      onChange={handleFileChange}
+                      display={"none"}
+                      accept=".docx"
+                    />
+                    <Button
+                      size={"xs"}
+                      type="button"
+                      boxShadow={"4px 4px 1px black"}
+                      border={"2px solid black"}
+                      variant="outline"
+                      position={"relative"}
+                      bottom={0}
+                      transition={"bottom 0.2s ease-out"}
+                      _hover={{ bottom: "4px" }}
+                      color={"black"}
+                      backgroundColor={"white"}
+                      cursor={"pointer"}
+                      p={"1em"}
+                      _active={{ backgroundColor: "none" }}
+                      onClick={handleSubmit}
+                      mt={"5px"}
+                    >
+                      Upload
+                    </Button>
                   </Box>
                 </Flex>
+              </form>
+            </Flex>
+          </Flex>
+          <Flex
+            flexWrap={"wrap"}
+            boxShadow={"4px 4px 1px black"}
+            border={"2px solid black"}
+            justifyContent={"center"}
+            alignItems={"center"}
+            maxW={"25%"}
+            minH={{ md: "", base: "25%" }}
+            minW={{ md: "25%", base: "100%" }}
+          >
+            <form>
+              <Flex
+                p={"5vw"}
+                alignItems="center"
+                flexWrap={"wrap"}
+                flexDirection={{ md: "column", base: "row" }}
+                gap={2}
+              >
                 <Box>
-                  <Input
-                    id={"upload"}
-                    border={"none"}
-                    type="file"
-                    onChange={handleFileChange}
-                    display={"none"}
-                    accept=".docx"
-                  />
-                  <Button
-                    size={"xs"}
-                    type="button"
+                  <Select
                     boxShadow={"4px 4px 1px black"}
                     border={"2px solid black"}
-                    variant="outline"
-                    position={"relative"}
-                    bottom={0}
-                    transition={"bottom 0.2s ease-out"}
-                    _hover={{ bottom: "4px" }}
-                    color={"black"}
+                    _hover={{ border: "2px solid black" }}
+                    focusBorderColor={"black"}
+                    cursor="pointer"
+                    placeholder="Group"
                     backgroundColor={"white"}
-                    cursor={"pointer"}
-                    p={"1em"}
-                    _active={{ backgroundColor: "none" }}
-                    onClick={handleSubmit}
-                    mt={"5px"}
+                    onChange={handleGroupChange}
+                    value={selectedGroup}
                   >
-                    Upload
+                    {teacher &&
+                      teacher.Unigroups.map((unigroup) => {
+                        return (
+                          <option key={unigroup.id} value={unigroup.id}>
+                            {unigroup.name}
+                          </option>
+                        );
+                      })}
+                  </Select>
+                </Box>
+                <Box>
+                  <Select
+                    boxShadow={"4px 4px 1px black"}
+                    border={"2px solid black"}
+                    _hover={{ border: "2px solid black" }}
+                    focusBorderColor={"black"}
+                    cursor="pointer"
+                    placeholder="Subject"
+                    backgroundColor={"white"}
+                    onChange={handleSubjectChange}
+                    value={selectedSubject}
+                  >
+                    {unigroup &&
+                      unigroup.Subjects.map((group) => (
+                        <option key={group.id} value={group.id}>
+                          {group.name}
+                        </option>
+                      ))}
+                  </Select>
+                </Box>
+                <Box>
+                  <Button
+                    boxShadow={"4px 4px 1px black"}
+                    border={"2px solid black"}
+                    _hover={{ border: "2px solid black" }}
+                    focusBorderColor={"black"}
+                    cursor="pointer"
+                    backgroundColor={"white"}
+                    type={"button"}
+                    alignContent={"center"}
+                    onClick={handleSearch}
+                  >
+                    Search
                   </Button>
                 </Box>
               </Flex>
             </form>
           </Flex>
         </Flex>
-        <Flex
-          flexWrap={"wrap"}
-          boxShadow={"4px 4px 1px black"}
-          border={"2px solid black"}
-          justifyContent={"center"}
-          alignItems={"center"}
-          maxW={"25%"}
-          minH={{ md: "", base: "25%" }}
-          minW={{ md: "25%", base: "100%" }}
-        >
-          <form>
-            <Flex
-              p={"5vw"}
-              alignItems="center"
-              flexWrap={"wrap"}
-              flexDirection={{ md: "column", base: "row" }}
-              gap={2}
-            >
-              <Box>
-                <Select
-                  boxShadow={"4px 4px 1px black"}
-                  border={"2px solid black"}
-                  _hover={{ border: "2px solid black" }}
-                  focusBorderColor={"black"}
-                  cursor="pointer"
-                  placeholder="Group"
-                  backgroundColor={"white"}
-                  onChange={handleGroupChange}
-                  value={selectedGroup}
-                >
-                  {teacher &&
-                    teacher.Unigroups.map((unigroup) => {
-                      return (
-                        <option key={unigroup.id} value={unigroup.id}>
-                          {unigroup.name}
-                        </option>
-                      );
-                    })}
-                </Select>
-              </Box>
-              <Box>
-                <Select
-                  boxShadow={"4px 4px 1px black"}
-                  border={"2px solid black"}
-                  _hover={{ border: "2px solid black" }}
-                  focusBorderColor={"black"}
-                  cursor="pointer"
-                  placeholder="Subject"
-                  backgroundColor={"white"}
-                  onChange={handleSubjectChange}
-                  value={selectedSubject}
-                >
-                  {unigroup &&
-                    unigroup.Subjects.map((group) => (
-                      <option key={group.id} value={group.id}>
-                        {group.name}
-                      </option>
-                    ))}
-                </Select>
-              </Box>
-              <Box>
-                <Button
-                  boxShadow={"4px 4px 1px black"}
-                  border={"2px solid black"}
-                  _hover={{ border: "2px solid black" }}
-                  focusBorderColor={"black"}
-                  cursor="pointer"
-                  backgroundColor={"white"}
-                  type={"button"}
-                  alignContent={"center"}
-                  onClick={handleSearch}
-                >
-                  Search
-                </Button>
-              </Box>
-            </Flex>
-          </form>
-        </Flex>
-      </Flex>
-    </div>
+      </div>
+    </>
   );
 }
