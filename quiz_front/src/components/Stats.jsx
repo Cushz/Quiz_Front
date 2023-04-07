0330a123eaf629644e2bfc395b6c793003d9408f
HorrorOTB <nicatabdullazade135@gmail.com>
2023-04-08 01:36:23 +0400

    fixed refresh bug in stats page

--- quiz_front/src/components/Stats.jsx
+++ quiz_front/src/components/Stats.jsx
@@ -27,7 +27,10 @@ export default function Stats(props) {
       );
       console.log(response);
     };
-    createResultData();
+    if (quizId) {
+      createResultData();
+    }
+    localStorage.removeItem("quizId");
   }, []);
 
   return (
